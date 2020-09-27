09912170e3fbad1deaaf5ebcfecfa158456d20c4
Swolf1997 <71954090+Swolf1997@users.noreply.github.com>
2020-09-27 18:54:50 -0500

    Update Display.java

--- Display.java
+++ Display.java
@@ -3,9 +3,14 @@ package project_1;
 import java.util.*;
 
 public class Display {
+	/**
+	 * Displays the movies in the linked list
+	 * @param list: list of movies
+	 */
 	public static void DisplayMovies(Deque<Movies> list) {
-		// Displaying the movies by using {it}
+		// Using {it} to move through the list
 		Iterator<Movies> it = list.iterator();
+		// Checking if there has an movies left
 		while (it.hasNext()) {
 			System.out.println(it.next());
 		}
