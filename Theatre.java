c14500146d0dbba4ccd98c9042810325c7885f3f
Elwood00 <55815936+Elwood00@users.noreply.github.com>
2020-09-27 16:27:39 -0500

    Update Theatre.java

--- Theatre.java
+++ Theatre.java
@@ -3,6 +3,7 @@ package sem2project1;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.Date;
 
 public class Theatre {
 	public static void main(String[] args) throws IOException {
@@ -12,6 +13,16 @@ public class Theatre {
 		
 	//Create scanner
 	Scanner scanner = new Scanner(System.in);
+		
+	//Create Date formatting
+	SimpleDateFormat ft = new SimpleDateFormat ("mm/dd/YYYY");
+		
+	//Temp Values for manipulation
+	String name = "";
+	String releaseDate = "";
+	String description = "";
+	String receiveDate = "";
+	String status = "";
 	
 	//Create file input/output stream
 	FileInputStream inputFile = new FileInputStream("Movies.txt");
