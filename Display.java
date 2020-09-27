277ee178a923920a89fc4bf41dad4ae0b14c8b02
Swolf1997 <71954090+Swolf1997@users.noreply.github.com>
2020-09-27 17:57:17 -0500

    Update Display.java

--- Display.java
+++ Display.java
@@ -5,7 +5,7 @@ import java.util.*;
 public class Display {
 	public static void DisplayMovies(Deque<Movies> list) {
 		// Displaying the movies by using {it}
-		new Iterator<Movies> it = list.iterator();
+		Iterator<Movies> it = list.iterator();
 		while (it.hasNext()) {
 			System.out.println(it.next());
 		}
