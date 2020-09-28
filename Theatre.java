8b3b7509cc0c8cc75a2e222f0e805a5bb3ca594a
Swolf1997 <71954090+Swolf1997@users.noreply.github.com>
2020-09-28 17:59:51 -0500

    Add files via upload

--- Theatre.java
+++ Theatre.java
@@ -1,9 +1,12 @@
-package sem2project1;
+package project_1;
 
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.Scanner;
-import java.util.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+
 
 public class Theatre {
 	public static void main(String[] args) throws IOException, ParseException {
@@ -11,31 +14,28 @@ public class Theatre {
 	//Create linked list
 	Deque<Movies> moviesReleased = new LinkedList<Movies>();
 	Deque<Movies> moviesReceived = new LinkedList<Movies>();
+	// For formating dates
+	SimpleDateFormat ft = new SimpleDateFormat("MM/dd/yyyy");
+	ft.setLenient(false);
 		
 	//Create scanner
 	Scanner scanner = new Scanner(System.in);
-		
-	//Create Date formatting
-	SimpleDateFormat ft = new SimpleDateFormat ("MM/dd/YYYY");
-	ft.setLenient(false);
-		
+	
+	//Create file input/output stream
+	FileInputStream inputFile = new FileInputStream("Movies.txt");
+	Scanner reader = new Scanner(inputFile);
+	
+	// Populating the lists, in ascending {releaseDate}
+	Add.addFile(moviesReleased, moviesReceived, reader, ft);
+	
 	//Temp Values for manipulation
 	String name = "";
 	String releaseDate = "";
 	String description = "";
 	String receiveDate = "";
 	
-	//Create file input/output stream
-	FileInputStream inputFile = new FileInputStream("Movies.txt");
-	Scanner reader = new Scanner(inputFile);
-		
-	// Populating the lists, is ascending {releaseDate}
-	Add.addFile(moviesReleased, moviesReceived, reader, ft);
 	
-	
-
-	
-	// Boolean to break loop
+	// Boolean for while loop
 	Boolean run = true;
 	while (run) {
 		//Create text based menu
@@ -51,14 +51,14 @@ public class Theatre {
 		if (option.toUpperCase().equals("D")) {
 			
 			System.out.println("You have selected to display movies.");
-			System.out.println("Movies released:");
+			System.out.println("Movies Released:");
 			Display.DisplayMovies(moviesReleased);
-			System.out.println("Movies received:");
+			System.out.println("Movies Received:");
 			Display.DisplayMovies(moviesReceived);
 			
 		} else if (option.toUpperCase().equals("A")) {
+			
 			System.out.println("You have selected to add a movie.");
-			//Code to add movie
 			System.out.println("Please enter the Movie's name.");
 			name = scanner.next();
 			System.out.println("Please enter the Movie's release date.");
@@ -68,7 +68,6 @@ public class Theatre {
 			System.out.println("Please enter the Movie's recieve date.");
 			receiveDate = scanner.next();
 			
-
 		} else if (option.toUpperCase().equals("S")) {
 			
 			System.out.println("You have selected to show movies with a given release date.");
@@ -77,30 +76,30 @@ public class Theatre {
 			Display.ReleaseMovies(moviesReleased, moviesReceived, relDate, ft);
 			
 		} else if (option.toUpperCase().equals("E")) {
+			
 			System.out.println("You have selected to edit movies.");
 			//Code to edit movies
+			
 		} else if (option.toUpperCase().equals("C")) {
 			
 			System.out.println("You have selected to count movies.");
 			System.out.print("Enter given date -> ");
-			String given;
+			String given = scanner.next();
 			Count.CountMovies(moviesReleased, moviesReceived, given, ft);
 			
 		} else if (option.toUpperCase().equals("X")) {
+			
 			System.out.println("You have selected to terminate the program.");
 			run = !run;
 			break;
+			
 		} else {
+			
 			System.out.println("You have selected an invalid option, please try again.");
 			continue;
+			
 		}
-		System.out.println("Please choose an option: ");
-		System.out.println("Press 'D' to display movies.");
-		System.out.println("Press 'A' to add a movie.");
-		System.out.println("Press 'S' to show movies with a given release date.");
-		System.out.println("Press 'E' to edit movies.");
-		System.out.println("Press 'C' to count movies.");
-		System.out.println("Press 'X' to terminate program.");
+		System.out.println();
 	}
 	
 	//Close Everything
